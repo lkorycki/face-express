11612dcf40f21a986cb95e9b707a94989ce61800
lkorycki <korycki.lukas@gmail.com>
2015-10-14 02:36:06 +0200

    Generic maxPos function added

--- src/MathCore.h
+++ src/MathCore.h
@@ -15,6 +15,24 @@ public:
     static int histThresh2D(Mat& mat, float p);
     static float wbParam2D(Mat& mat);
 
+    template<typename Type>
+    static int maxPos(Type arr[], int size)
+    {
+        int maxIdx = 0;
+        Type maxVal = arr[0];
+
+        for(int i = 1; i < size; i++)
+        {
+            if(arr[i] > maxVal)
+            {
+                maxIdx = i;
+                maxVal = arr[i];
+            }
+        }
+
+        return maxIdx;
+    }
+
 };
 
 #endif // PROCORES_H
